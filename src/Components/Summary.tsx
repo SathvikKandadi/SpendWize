05360c5515fddf9dce1a1fba86c1015677a3453b
Sathvik Kandadi <sathvikkandadi@gmail.com>
2024-03-20 22:57:32 +0530

    UI changes

--- src/Components/Summary.tsx
+++ src/Components/Summary.tsx
@@ -3,11 +3,29 @@ import SpendCard from './SpendCard'
 
 export default function Summary() {
     return (
-        <div className='w-screen h-screen flex flex-row justify-center items-center'>
+        <div className='w-full h-full'>
             <div className=''>
                 <div>Spending Summary</div>
-                <div>See where your money is going exactly </div>
-                <SpendCard></SpendCard>
+                <div className='flex justify-between'>
+                    <div>See where your money is going exactly</div>
+                    <div className='border-2 rounded p-2'><input placeholder='Select Year'></input></div>
+                </div>
+                <div className='grid grid-cols-4 gap-2 w-full'>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    <div className='col-span-1'><SpendCard></SpendCard></div>
+                    
+                </div>
+                
             </div>
         </div>
 
